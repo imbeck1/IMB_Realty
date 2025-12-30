70d5feddad68bd019c34d7af9e3e1fea56ed9181
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 15:13:50 -0500

    Update IMB_RealtyContext.cs

--- IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
+++ IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
@@ -1,15 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 
-public class IMB_RealtyContext : DbContext
+namespace IMB_Realty.Api.Features.Persistence.Data
 {
-    public DbSet<House> Houses => Set<House>();
-
-    public IMB_RealtyContext(DbContextOptions<IMB_RealtyContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class IMB_RealtyContext : DbContext
     {
-        base.OnModelCreating(modelBuilder);
+        public IMB_RealtyContext(DbContextOptions<IMB_RealtyContext> options)
+            : base(options)
+        {
+        }
 
-        modelBuilder.ApplyConfiguration(new HouseConfig());
+        public DbSet<House> Houses { get; set; }
+    }
+
+    public class House
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Image { get; set; }
+        public string Location { get; set; } = default!;
+        public int Price { get; set; }
+        public string Description { get; set; } = default!;
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public int SquareFeet { get; set; }
     }
 }
