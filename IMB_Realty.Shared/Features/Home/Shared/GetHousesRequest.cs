53d8f472afdc25112616052a13a9e46f18fca16c
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 14:03:31 -0500

    Update GetHousesRequest.cs

--- IMB_Realty.Shared/Features/Home/Shared/GetHousesRequest.cs
+++ IMB_Realty.Shared/Features/Home/Shared/GetHousesRequest.cs
@@ -1,11 +1,23 @@
-namespace IMB_Realty.Shared.Features.Home.Shared.GetHousesRequest;
-
 using MediatR;
+
+namespace IMB_Realty.Shared.Features.Home.Shared.GetHousesRequest;
 
 public record GetHousesRequest : IRequest<GetHousesRequest.Response>
 {
-    public const string RouteTemplate = "/api/houses";
+    // IMPORTANT: no leading slash
+    public const string RouteTemplate = "api/houses";
 
-    public record House(int Id, string Name, string? Image, string Location, int Price, string Description, int Bedrooms, int Bathrooms, int SquareFeet);
+    public record House(
+        int Id,
+        string Name,
+        string? Image,
+        string Location,
+        decimal Price,
+        string Description,
+        int Bedrooms,
+        int Bathrooms,
+        int SquareFeet
+    );
+
     public record Response(IEnumerable<House> Houses);
 }
