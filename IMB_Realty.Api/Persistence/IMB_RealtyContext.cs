e9103b17a54480734da6a3c79f827d4fdb1df859
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 15:33:17 -0500

    Update IMB_RealtyContext.cs

--- IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
+++ IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
@@ -1,27 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using IMB_Realty.Api.Persistence.Data.Entities;
 
-namespace IMB_Realty.Api.Features.Persistence
+namespace IMB_Realty.Api.Persistence
 {
     public class IMB_RealtyContext : DbContext
     {
         public IMB_RealtyContext(DbContextOptions<IMB_RealtyContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         public DbSet<House> Houses { get; set; }
-    }
-
-    public class House
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = default!;
-        public string? Image { get; set; }
-        public string Location { get; set; } = default!;
-        public int Price { get; set; }
-        public string Description { get; set; } = default!;
-        public int Bedrooms { get; set; }
-        public int Bathrooms { get; set; }
-        public int SquareFeet { get; set; }
     }
 }
