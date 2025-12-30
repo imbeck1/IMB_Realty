9a1b8af7498915a6ac8b0fa57fe18500b79235de
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 16:48:09 -0500

    Update HouseDto.cs

--- IMB_Realty.Shared/Features/ManageHouses/Shared/HouseDto.cs
+++ IMB_Realty.Shared/Features/ManageHouses/Shared/HouseDto.cs
@@ -1,48 +1,33 @@
 using FluentValidation;
-namespace IMB_Realty.Shared.Features.ManageHouses;
+using Microsoft.AspNetCore.Http;
 
+namespace IMB_Realty.Shared.Features.ManageHouses;
 
 public class HouseDto
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = "";
+
     public string Description { get; set; } = "";
+
     public int Price { get; set; }
-    public string Location
-    {
-        get;
-        set;
-    } = "";
 
-    public int Bedrooms
-    {
-        get;
-        set;
-    }
+    public string Location { get; set; } = "";
 
-    public int Bathrooms
-    {
-        get;
-        set;
-    }
+    public int Bedrooms { get; set; }
 
-    public int SquareFeet
-    {
-        get;
-        set;
-    }
+    public int Bathrooms { get; set; }
 
-    public string? Image
-    {
-        get;
-        set;
-    }
+    public int SquareFeet { get; set; }
 
-    public ImageAction ImageAction
-    {
-        get;
-        set;
-    }
+    // Image file uploaded by the user
+    public IFormFile? ImageFile { get; set; }
+
+    // Optional base64 representation for displaying existing images
+    public string? ImageBase64 { get; set; }
+
+    public ImageAction ImageAction { get; set; } = ImageAction.None;
 }
 
 public enum ImageAction
@@ -51,8 +36,6 @@ public enum ImageAction
     Add,
     Remove
 }
-
-
 
 public class HouseValidator : AbstractValidator<HouseDto>
 {
@@ -65,5 +48,11 @@ public class HouseValidator : AbstractValidator<HouseDto>
         RuleFor(x => x.Bathrooms).NotEmpty().WithMessage("Please enter Bathroom number");
         RuleFor(x => x.SquareFeet).NotEmpty().WithMessage("Please enter Square Footage");
         RuleFor(x => x.Price).NotEmpty().WithMessage("Please enter a Price");
+        
+        // Optional: validate uploaded image file size or type
+        RuleFor(x => x.ImageFile)
+            .Must(f => f == null || f.Length > 0)
+            .WithMessage("If provided, the image file must not be empty.");
     }
 }
+
