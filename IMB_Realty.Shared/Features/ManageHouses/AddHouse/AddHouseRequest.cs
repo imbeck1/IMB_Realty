8909344c130a1b51611ef86b2e2ebbd4a87c56c8
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 17:00:47 -0500

    Update AddHouseRequest.cs

--- IMB_Realty.Shared/Features/ManageHouses/AddHouse/AddHouseRequest.cs
+++ IMB_Realty.Shared/Features/ManageHouses/AddHouse/AddHouseRequest.cs
@@ -2,14 +2,16 @@ using System.Net;
 using FluentValidation;
 using IMB_Realty.Shared.Features.ManageHouses;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 
-public record AddHouseRequest(HouseDto House, IFormFile? Image) : IRequest<AddHouseRequest.Response>
+namespace IMB_Realty.Shared.Features.ManageHouses;
+
+public record AddHouseRequest(HouseDto House) : IRequest<AddHouseRequest.Response>
 {
     public const string RouteTemplate = "/api/houses";
 
     public record Response(int HouseId);
 }
+
 
 public class AddHouseRequestValidator : AbstractValidator<AddHouseRequest>
 {
