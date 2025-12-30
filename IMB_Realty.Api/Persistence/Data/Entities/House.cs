d368c92d506d8c25bd364b4b773112beb30744d2
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 16:39:12 -0500

    Update House.cs

--- IMB_Realty.Api/Persistence/Data/Entities/House.cs
+++ IMB_Realty.Api/Persistence/Data/Entities/House.cs
@@ -3,62 +3,26 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace IMB_Realty.Api.Persistence.Data.Entities;
 
-
 public class House
 {
-    public int Id
-    {
-        get;
-        set;
-    }
+    public int Id { get; set; }
 
-    public string Name
-    {
-        get;
-        set;
-    } = "";
+    public string Name { get; set; } = "";
 
-    public int Price
-    {
-        get;
-        set;
-    }
+    public int Price { get; set; }
 
-    public string Description
-    {
-        get;
-        set;
-    } = "";
+    public string Description { get; set; } = "";
 
-    public string? Image
-    {
-        get;
-        set;
-    } = "";
+    // Store images as bytes in the database
+    public byte[]? Image { get; set; }
 
-    public string Location
-    {
-        get;
-        set;
-    } = "";
+    public string Location { get; set; } = "";
 
-    public int Bedrooms
-    {
-        get;
-        set;
-    }
+    public int Bedrooms { get; set; }
 
-    public int Bathrooms
-    {
-        get;
-        set;
-    }
+    public int Bathrooms { get; set; }
 
-    public int SquareFeet
-    {
-        get;
-        set;
-    }
+    public int SquareFeet { get; set; }
 }
 
 public class HouseConfig : IEntityTypeConfiguration<House>
@@ -72,5 +36,11 @@ public class HouseConfig : IEntityTypeConfiguration<House>
         builder.Property(x => x.Bathrooms).IsRequired();
         builder.Property(x => x.SquareFeet).IsRequired();
         builder.Property(x => x.Price).IsRequired();
+
+        // Configure Image column to allow nulls
+        builder.Property(x => x.Image)
+               .HasColumnType("VARBINARY(MAX)")
+               .IsRequired(false);
     }
 }
+
