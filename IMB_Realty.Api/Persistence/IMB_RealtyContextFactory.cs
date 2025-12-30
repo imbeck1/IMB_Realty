4563be225c11531201abc6fad627dcf76cd86195
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 15:33:35 -0500

    Update IMB_RealtyContextFactory.cs

--- IMB_Realty.Api/Persistence/IMB_RealtyContextFactory.cs
+++ IMB_Realty.Api/Persistence/IMB_RealtyContextFactory.cs
@@ -1,25 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 
-public class IMB_RealtyContextFactory : IDesignTimeDbContextFactory<IMB_RealtyContext>
+namespace IMB_Realty.Api.Persistence
 {
-    public IMB_RealtyContext CreateDbContext(string[] args)
+    public class IMB_RealtyContextFactory : IDesignTimeDbContextFactory<IMB_RealtyContext>
     {
-        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../IMB_Realty.Api");
-
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(basePath)
-            .AddJsonFile("appsettings.json", optional: false)
-            .Build();
-
-        var optionsBuilder = new DbContextOptionsBuilder<IMB_RealtyContext>();
-        var connectionString = configuration.GetConnectionString("IMB_RealtyContext");
-
-        optionsBuilder.UseSqlite(connectionString);
-
-        return new IMB_RealtyContext(optionsBuilder.Options);
+        public IMB_RealtyContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IMB_RealtyContext>();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Data", "imbrealty.db");
+            optionsBuilder.UseSqlite($"Data Source={path}");
+            return new IMB_RealtyContext(optionsBuilder.Options);
+        }
     }
 }
 
