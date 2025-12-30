a54f28850b3dd6a93aae68e2545cec0c64c98519
imbeck1 <143744983+imbeck1@users.noreply.github.com>
2025-12-30 16:35:44 -0500

    Update IMB_RealtyContext.cs

--- IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
+++ IMB_Realty.Api/Persistence/IMB_RealtyContext.cs
@@ -6,8 +6,21 @@ namespace IMB_Realty.Api.Persistence
     public class IMB_RealtyContext : DbContext
     {
         public IMB_RealtyContext(DbContextOptions<IMB_RealtyContext> options)
-            : base(options) { }
+            : base(options)
+        {
+        }
 
         public DbSet<House> Houses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Store image as VARBINARY(MAX) in SQL
+            modelBuilder.Entity<House>()
+                .Property(h => h.Image)
+                .HasColumnType("VARBINARY(MAX)");
+        }
     }
 }
+
